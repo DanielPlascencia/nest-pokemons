a31f6497722df67d640bab1461b116008238a11d
CJavatX <cdpm98@hotmail.com>
2024-03-04 15:23:10 -0600

    vercel.json - added_11

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api/v2'); // Funciona para poner un prefijo global en la aplicación. Ejemplo: /'api'/las-demas-rutas
+  // app.setGlobalPrefix('api/v2'); // Funciona para poner un prefijo global en la aplicación. Ejemplo: /'api'/las-demas-rutas
 
   app.useGlobalPipes(
     new ValidationPipe({
